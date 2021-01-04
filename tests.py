07473c21de8d238c91a3fec1debee9a32d3ce7c4
Your Name <you@example.com>
2021-01-04 16:00:10 +0300

    Add tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
+from db import DB
 
 
 class Tester(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.db = DB(file_name="test_data.sqlite")
+        # TODO Add table creation and fake data generation

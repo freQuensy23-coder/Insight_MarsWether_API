51c845f6e6323913e7d1dec0f36bf0bbcbb385df
Your Name <you@example.com>
2021-01-04 15:55:42 +0300

    "Openingg connection in the same thread" - bug with sqlite fixed

--- db.py
+++ db.py
@@ -4,30 +4,34 @@ import time
 
 logger = logging.getLogger("db_logger")
 
+
 class DB:
-    def __init__(self, file_name = "data.sqlite"):
+    def __init__(self, file_name="data.sqlite"):
         self.file_name = file_name
         self.connection = sqlite3.connect(database="data.sqlite")
         # TODO Logging
 
     def add_new_day_info(self, info: str, day: int):
-        cur = self.connection.cursor()
-        info = info.replace("'", '"')
-        q = f"""INSERT into days_info (day, information, creation_time) VALUES ({day}, '{info}, {time.time()}')"""
-        logger.debug(f"Add new day info. q = {q}`")
-        cur.execute(q)
-        self.connection.commit()
+        with sqlite3.connect(self.file_name) as con:
+            cur = con.cursor()
+            info = info.replace("'", '"')
+            q = f"""INSERT into days_info (day, information, creation_time) VALUES ({day}, '{info}, {time.time()}')"""
+            logger.debug(f"Add new day info. q = {q}`")
+            cur.execute(q)
+            self.connection.commit()
 
-    def get_day_info(self, day: int)-> str:
-        cur = self.connection.cursor()
-        q = f"""SELECT * FROM days_info WHERE day = {day}"""
-        cur.execute(q)
-        res = cur.fetchone()
-        return res
+    def get_day_info(self, day: int) -> str:
+        with sqlite3.connect(self.file_name) as con:
+            cur = con.cursor()
+            q = f"""SELECT * FROM days_info WHERE day = {day}"""
+            cur.execute(q)
+            res = cur.fetchone()
+            return res
 
     def get_days_info(self, day_start, day_end):
-        cur = self.connection.cursor()
-        q = f"""SELECT * FROM days_info WHERE day >= {day_start} AND day <= {day_end}"""
-        cur.execute(q)
-        res = cur.fetchall()
-        return res
+        with sqlite3.connect(self.file_name) as con:
+            cur = con.cursor()
+            q = f"""SELECT * FROM days_info WHERE day >= {day_start} AND day <= {day_end}"""
+            cur.execute(q)
+            res = cur.fetchall()
+            return res
